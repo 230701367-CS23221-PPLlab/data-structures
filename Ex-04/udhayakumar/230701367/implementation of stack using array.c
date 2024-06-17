cf1f1fbaad74cbc8ba95d770d9c148f2d62c00ce
Udhayakumar.V <230701367@rajalakshmi.edu.in>
2024-06-17 18:59:23 +0530

    Update implementation of stack using array.c

--- Ex-04/udhayakumar/230701367/implementation of stack using array.c
+++ Ex-04/udhayakumar/230701367/implementation of stack using array.c
@@ -1,4 +1,8 @@
-## udhayakumar 230701367 implementation of stack using array
+NAME : UDHAYAKUMAR V
+ROLL NO : 230701367
+EX-04 : implementation of stack using array
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
